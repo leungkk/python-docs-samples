d76dde2ee76cb34433f539f06eef47ff3a3da3d9
K K LEUNG <34422441+leungkk@users.noreply.github.com>
2019-06-26 22:32:58 +0800

    Update snippets.py

--- storage/cloud-client/snippets.py
+++ storage/cloud-client/snippets.py
@@ -6,13 +6,13 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#    http://www.apache.org/licenses/LICENSE-2.0
+#    http://www.apache.org/licenses/LICENSE-2.0  //
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
-# limitations under the License.
+# limitations under the License.  
 
 """This application demonstrates how to perform basic operations on blobs
 (objects) in a Google Cloud Storage bucket.
@@ -25,7 +25,7 @@ import argparse
 import datetime
 import pprint
 
-# [START storage_upload_file]
+# [START storage_upload_file]  
 from google.cloud import storage
 
 # [END storage_upload_file]
